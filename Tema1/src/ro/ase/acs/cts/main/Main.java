f5b836c1d886d1123534058d0615fdded427419a
tudorman <tudormanole20@yahoo.com>
2023-03-09 12:58:18 +0200

    S.O.L.I.D. principles applied

--- Tema1/src/ro/ase/acs/cts/main/Main.java
+++ Tema1/src/ro/ase/acs/cts/main/Main.java
@@ -1,7 +1,12 @@
 package ro.ase.acs.cts.main;
 
+import ro.ase.acs.cts.classes.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Code cleanup");
+        Orchestrator orchestrator = new Orchestrator(new DBConnection(), new TableCreation(), new DBInsertion(), new DBRead(), new DBPrint());
+        orchestrator.run();
+        orchestrator.setDbInserter(new DBInsertionParams());
+        orchestrator.run();
     }
 }
